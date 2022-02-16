cb1461f2e469106c768361e60e7596aed971c2cb
Onur AĞA <onuraga@windowslive.com>
2022-02-16 14:39:19 +0300

    advemced vs...

--- AdvancedRepository/Controllers/CustomersController.cs
+++ AdvancedRepository/Controllers/CustomersController.cs
@@ -1,5 +1,6 @@
 ﻿using AdvancedRepository.Business;
 using AdvancedRepository.Models;
+using AdvancedRepository.Repository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,9 +11,45 @@ namespace AdvancedRepository.Controllers
 {
     public class CustomersController : Controller
     {
-        
+    
+        MyRepository.CustomersRepository my = new MyRepository.CustomersRepository();
+        CustomerModel cm = new CustomerModel();
         public ActionResult Index()
         {
+            cm.clist = my.GetList();
+            return View(cm);
+        }
+        public ActionResult Detay(string id)
+        {
+            cm.customers = my.Bul(id);
+            return View(cm);
+        }
+        public ActionResult Sil(string id)
+        {
+            Customers cm = my.Bul(id);
+            my.Delete(cm);
+            my.Save();
+            return RedirectToAction("Index");
+        }
+        
+        public ActionResult Guncel(string id)
+        {
+            cm.customers = my.Bul(id);
+            
+            return View(cm);
+        }
+        [HttpPost]
+        public ActionResult Guncel(string id,CustomerModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Customers sec = my.Bul(id);
+                sec.CompanyName = model.customers.CompanyName;
+                sec.ContactName = model.customers.ContactName;
+                sec.City = model.customers.City;
+                my.Save();
+                return RedirectToAction("Index");
+            }
             return View();
         }
     }
---
+++ AdvancedRepository/Models/CustomerModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdvancedRepository.Models
+{
+    public class CustomerModel
+    {
+        public Customers customers { get; set; }
+        public List<Customers> clist { get; set; }
+        public IEnumerable<SelectListItem> Listele { get; set; }
+    }
+}
